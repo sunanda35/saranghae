cc8c32dbb5d946c2828ab79ce71bfc4ce3d117c5
sunanda35 <sunandasamanta38@gmail.com>
2021-07-02 23:40:41 +0530

    profile register page slightly done

--- lib/pages/login/login_register.dart
+++ lib/pages/login/login_register.dart
@@ -8,13 +8,24 @@ class LoginProfileRegister extends StatefulWidget {
 }
 
 class _LoginProfileRegisterState extends State<LoginProfileRegister> {
-  final _fNameController = TextEditingController();
-  final _lNameController = TextEditingController();
+  final _nameController = TextEditingController();
+  final _aboutController = TextEditingController();
+  final _jobTitleController = TextEditingController();
+  final _companyController = TextEditingController();
+  final _schoolController = TextEditingController();
+  final _locationController = TextEditingController();
+  String sex = "Male";
+  List<dynamic> sexOriantation = [];
+  List<dynamic> passion = [];
 
   @override
   void dispose() {
-    _fNameController.dispose();
-    _lNameController.dispose();
+    _nameController.dispose();
+    _aboutController.dispose();
+    _jobTitleController.dispose();
+    _companyController.dispose();
+    _schoolController.dispose();
+    _locationController.dispose();
     super.dispose();
   }
 
@@ -25,10 +36,6 @@ class _LoginProfileRegisterState extends State<LoginProfileRegister> {
         return new Future(() => false);
       },
       child: Scaffold(
-        appBar: AppBar(
-          title: Text('Saranghae'),
-          automaticallyImplyLeading: false,
-        ),
         body: GestureDetector(
           onTap: () {
             FocusScope.of(context).unfocus();
@@ -38,86 +45,107 @@ class _LoginProfileRegisterState extends State<LoginProfileRegister> {
               mainAxisAlignment: MainAxisAlignment.start,
               crossAxisAlignment: CrossAxisAlignment.stretch,
               children: [
+                SizedBox(
+                  height: 45,
+                ),
+                Text(
+                  "Tell us about you.",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 28,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
                 Container(
                   width: MediaQuery.of(context).size.width,
-                  child: Text("First Container"),
-                ),
-                Center(
-                  child: Container(
-                    width: MediaQuery.of(context).size.width * 0.9,
-                    padding: EdgeInsets.only(left: 6, right: 6),
-                    decoration: BoxDecoration(
-                      color: Colors.black12,
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                    child: TextField(
-                      controller: _fNameController,
-                      decoration: InputDecoration(
-                        border: InputBorder.none,
-                        hintText: "Your First Name",
+                  padding: EdgeInsets.only(left: 6, right: 6),
+                  decoration: BoxDecoration(
+                    color: Colors.black12,
+                    // borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: TextField(
+                    controller: _nameController,
+                    decoration: InputDecoration(
+                      prefixIcon: Icon(
+                        Icons.person_outline,
+                        color: Colors.black,
                       ),
+                      border: InputBorder.none,
+                      hintText: "Enter Your Name",
                     ),
                   ),
                 ),
                 SizedBox(
                   height: 20,
                 ),
-                Center(
-                  child: Container(
-                    width: MediaQuery.of(context).size.width * 0.9,
-                    padding: EdgeInsets.only(left: 6, right: 6),
-                    decoration: BoxDecoration(
-                      color: Colors.black12,
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                    child: TextField(
-                      controller: _lNameController,
-                      decoration: InputDecoration(
-                        border: InputBorder.none,
-                        hintText: "Your Last Name",
-                      ),
+                Text(
+                  "About you:",
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Container(
+                  width: MediaQuery.of(context).size.width,
+                  padding: EdgeInsets.only(left: 6, right: 6),
+                  decoration: BoxDecoration(
+                    color: Colors.black12,
+                    // borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: TextField(
+                    controller: _aboutController,
+                    maxLines: 3,
+                    decoration: InputDecoration(
+                      border: InputBorder.none,
+                      hintText:
+                          "I like to sleep early, I like to get up late; I like to be alone, I like to be surrounded by people....",
                     ),
                   ),
                 ),
                 SizedBox(
                   height: 20,
                 ),
-                Center(
-                  child: Container(
-                    width: MediaQuery.of(context).size.width * 0.9,
-                    padding: EdgeInsets.only(left: 6, right: 6),
-                    decoration: BoxDecoration(
-                      color: Colors.black12,
-                      borderRadius: BorderRadius.circular(10),
+                Text(
+                  "Gender:",
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Container(
+                  width: MediaQuery.of(context).size.width,
+                  padding: EdgeInsets.only(left: 6, right: 6),
+                  decoration: BoxDecoration(
+                    color: Colors.black12,
+                    // borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: DropdownButton<String>(
+                    value: sex,
+                    elevation: 16,
+                    style: const TextStyle(color: Colors.deepPurple),
+                    underline: Container(
+                      height: 2,
+                      color: Colors.deepPurpleAccent,
                     ),
-                    child: TextField(
-                      controller: _lNameController,
-                      decoration: InputDecoration(
-                        border: InputBorder.none,
-                        hintText: "Your Last Name",
-                      ),
-                    ),
+                    onChanged: (String? newValue) {
+                      setState(() {
+                        sex = newValue!;
+                      });
+                    },
+                    items: <String>['Male', 'Female', 'Others']
+                        .map<DropdownMenuItem<String>>((String value) {
+                      return DropdownMenuItem<String>(
+                        value: value,
+                        child: Text(value),
+                      );
+                    }).toList(),
                   ),
                 ),
                 SizedBox(
                   height: 20,
-                ),
-                Center(
-                  child: Container(
-                    width: MediaQuery.of(context).size.width * 0.9,
-                    padding: EdgeInsets.only(left: 6, right: 6),
-                    decoration: BoxDecoration(
-                      color: Colors.black12,
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                    child: TextField(
-                      controller: _lNameController,
-                      decoration: InputDecoration(
-                        border: InputBorder.none,
-                        hintText: "Your Last Name",
-                      ),
-                    ),
-                  ),
                 ),
               ],
             ),
