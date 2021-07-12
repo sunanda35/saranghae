8a19514eb92c68852768f96fcb139694527b67cf
sunanda35 <sunandasamanta60@gmail.com>
2021-07-12 06:21:41 +0530

    new setting check

--- lib/main.dart
+++ lib/main.dart
@@ -5,13 +5,13 @@ import 'package:saranghae/pages/login/login_page.dart';
 import 'package:saranghae/pages/login/login_profile_pic.dart';
 import 'package:saranghae/pages/login/login_register.dart';
 import 'package:saranghae/pages/profile/profile_page.dart';
+import 'package:authentication/authentication.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -19,15 +19,16 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primaryColor: Color(0xFFf8f8ff),
       ),
-      initialRoute: 'chatPage',
+      initialRoute: 'auth',
       routes: {
+        'auth': (context) =>
+            Authentication(), // tried new position settings in flutter(ADVANCE CONCEPT)
         'login': (context) => LogInPage(),
         'profileRegister': (context) => LoginProfileRegister(),
         'profilePhotoRegister': (context) => LoginProfilePhoto(),
         'profilePage': (context) => ProfilePage(),
         'chatPage': (context) => ChatScreen(),
       },
-      // home: LogInPage(),sldkjfj
     );
   }
 }
