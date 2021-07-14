c9ce46c0a77c8e64a5dea5082fe8f72ae0a106e2
SHUBHANKAR <50442880+ShubhP23@users.noreply.github.com>
2021-07-14 20:30:46 +0530

    Revert "Package"

--- lib/constant.dart
+++
@@ -1,4 +0,0 @@
-import 'package:flutter/material.dart';
-
-const Color kAPP_BG_COLOR = Color(0xFFffffff);
-const String kAPP_NAME = "Saranghae";
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:saranghae/constant.dart';
 import 'package:saranghae/pages/login/login_page.dart';
-import 'package:saranghae/pages/login/login_profile_pic.dart';
-import 'package:saranghae/pages/login/login_register.dart';
-import 'package:saranghae/pages/profile/profile_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -14,19 +10,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: kAPP_NAME,
+      title: 'Saranghae',
       theme: ThemeData(
         primaryColor: Color(0xFFf8f8ff),
       ),
-      initialRoute: 'profilePhotoRegister', //sunanda
-      // initialRoute: 'profilePage', //shubankar
-      routes: {
-        'login': (context) => LogInPage(),
-        'profileRegister': (context) => LoginProfileRegister(),
-        'profilePhotoRegister': (context) => LoginProfilePhoto(),
-        'profilePage': (context) => ProfilePage(),
-      },
-      // home: LogInPage(),
+      home: LogInPage(),
     );
   }
 }
